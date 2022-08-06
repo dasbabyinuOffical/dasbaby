21b60be3b145fe249380c56fdcea3031be0d1e0d
Cong Liu <cong.liu@ip-192-168-6-11.ap-northeast-1.compute.internal>
2022-08-06 13:33:00 +0800

    add new image

--- src/Nft.js
+++ src/Nft.js
@@ -1,84 +1,89 @@
-import React, { Component } from 'react'
-import Masonry from 'masonry-layout'
-import axios from 'axios'
+import React, { Component } from "react";
+import Masonry from "masonry-layout";
+import axios from "axios";
 export default class Pinterest extends Component {
-    state = {
-        data: [],
-        butitleArrr: [
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-            '领取',
-        ]
+  state = {
+    data: [],
+    butitleArrr: [
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+      "领取",
+    ],
+  };
 
-    }
+  componentDidMount() {
+    axios.get("http://dasbabyinu.com/nft/").then((res) => {
+      this.setState(
+        {
+          data: res.data,
+        },
+        () => {
+          this.advanceWidth();
+        }
+      );
+    });
+  }
 
-    componentDidMount() {
-        axios.get('http://dasbabyinu.com/nft/')
-            .then(res => {
-                this.setState({
-                    data: res.data
-                }, () => {
-                    this.advanceWidth()
-                })
-            })
-    }
+  advanceWidth = () => {
+    var elem = document.querySelector(".pages_hoc");
+    new Masonry(elem, {
+      itemSelector: ".imgBox",
+      columnWidth: ".imgBox",
+      fitWidth: true,
+      gutter: 20,
+    });
+  };
 
-    advanceWidth = () => {
-        var elem = document.querySelector('.pages_hoc')
-        new Masonry(elem, {
-            itemSelector: '.imgBox',
-            columnWidth: '.imgBox',
-            fitWidth: true,
-            gutter: 20
-        })
-    }
+  getNft = (index) => {
+    this.state.butitleArrr[index] = "已领取";
+    this.setState({
+      butitleArrr: this.state.butitleArrr,
+    });
+  };
 
-    getNft = (index) => {
-        this.state.butitleArrr[index] = '已领取'
-        this.setState({
-            butitleArrr: this.state.butitleArrr
-        })
-    }
+  render() {
+    const { data, butitleArrr } = this.state;
 
-    render() {
-        const { data, butitleArrr } = this.state
+    return (
+      <div>
+        <div>
+          <div className="owl-prev">NFT Marketplace</div>
+          <form>
+            <button
+              className="owl-next btn btn--accent2 text--small text--medium btn-top-app"
+              formAction="https://metamask.io/buy-crypto/"
+            >
+              Connect Wallet
+            </button>
+          </form>
+        </div>
 
-        return (
-            <div>
-                <div>
-                    <div className='owl-prev'>DasBaby NFT市场</div>
-                    <form>
-                        <button
-                            className="owl-next btn btn--accent2 text--small text--medium btn-top-app"
-                            formAction="https://metamask.io/buy-crypto/">
-                            Connect Wallet
-                        </button>
-                    </form>
+        <div className="pages_pinterest">
+          <div className="pages_hoc">
+            {data.map((item, index) => {
+              return (
+                <div key={index} className="imgBox">
+                  <img src={item.image} />
+                  <p className="nftNumber">{item.name}</p>
+                  <button
+                    className="nftButton"
+                    onClick={() => this.getNft(index)}
+                  >
+                    {butitleArrr[index]}
+                  </button>
                 </div>
-
-                <div className='pages_pinterest'>
-                    <div className="pages_hoc">
-                        {
-                            data.map((item, index) => {
-                                return (
-                                    <div key={index} className='imgBox'>
-                                        <img src={item.image} />
-                                        <p className='nftNumber'>{item.name}</p>
-                                        <button className='nftButton' onClick={() => this.getNft(index)}>{butitleArrr[index]}</button>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
