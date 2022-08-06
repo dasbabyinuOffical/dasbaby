ce26ef6e7f92aacd94066707318e15f3073a002b
Cong Liu <cong.liu@ip-192-168-6-10.ap-northeast-1.compute.internal>
2022-08-06 22:38:02 +0800

    add metamask

--- src/Nft.js
+++ src/Nft.js
@@ -1,33 +1,38 @@
 import React, { useState,useEffect } from "react";
 import axios from "axios";
+import MetaMask from "./MetaMask";
+import { ethers,BigNumber } from "ethers";
+import Genesis from "./Genesis.json";
 
 function NFT() {
+  const contract = "0x74155e8E00D19083033d3f58C0BA25eAE1856f84"
   const [data,setData] = useState([])
-
-  const [scrollY,setScrollY] = useState(0)
-
   useEffect(() => {
-    axios.get("http://dasbabyinu.com/nft/?start=1&&page=100").then((res) => {
+    axios.get("http://dasbabyinu.com/nft/?start=1&&page=14").then((res) => {
       setData(res.data)
   })
   },[])
 
-  const mintNft = () =>{
+  const mintNft = async (index) =>{
+    if (!window.ethereum){
+      return 
+    }
+    const providerWeb3 = new ethers.providers.Web3Provider(window.ethereum);
+    const signer =  providerWeb3.getSigner();
+    const daiContract = new ethers.Contract(contract, Genesis, providerWeb3);
+    const daiContractWithSigner = daiContract.connect(signer);
 
+    const id = BigNumber.from(index);
+    const transaction  = await daiContractWithSigner.mint(id)
+    const transactionReceipt = await transaction.wait();
+    console.log(transactionReceipt)
   }
 
     return (
       <div>
         <div>
           <div className="owl-prev">NFT Marketplace</div>
-          <form>
-            <button
-              className="owl-next btn btn--accent2 text--small text--medium btn-top-app"
-              formAction="https://metamask.io/buy-crypto/"
-            >
-              Connect Wallet
-            </button>
-          </form>
+          <MetaMask/>
         </div>
 
         <div className="pages_pinterest"> 
@@ -40,7 +45,7 @@ function NFT() {
                   
                    {item.owner === "0" && <button
                     className="nftButton"
-                    onClick={mintNft}
+                    onClick={() => mintNft(index)}
                   >
                    Mint
                   </button>} 
